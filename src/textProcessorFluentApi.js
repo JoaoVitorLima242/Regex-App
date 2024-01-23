4bbe448c25a61d3deecda069274f57ade3247c8f
JoaoVitorLima242 <jlima0500@gmail.com>
2024-01-23 15:11:52 -0300

    feat: extracting people from pdf

--- src/textProcessorFluentApi.js
+++ src/textProcessorFluentApi.js
@@ -4,6 +4,15 @@ class TextProcessorFluentApi {
     this.#content = content;
   }
 
+  extractPeopleData() {
+    const matchPerson = /(?<=[CONTRANTE|CONTRATADA]:\s{1})(?!\s)(.*\n.*)$/gim;
+    const onlyPerson = this.#content.match(matchPerson);
+
+    this.#content = onlyPerson;
+
+    return this;
+  }
+
   build() {
     return this.#content;
   }
